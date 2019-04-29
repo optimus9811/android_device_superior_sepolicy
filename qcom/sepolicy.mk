2de088075c9f0329ed4849d16407ecfc3c143c30
Jayant Deshmukh <jayantdeshmuk008@gmail.com>
2019-04-29 17:40:53 +0530

    sepolicy: Changes for SuperiorOS
    Change-Id: I328ca1720e068429bd0c985a990d681ccb62f081

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from superior
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/superior/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/superior/sepolicy/qcom/common \
+    device/superior/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
